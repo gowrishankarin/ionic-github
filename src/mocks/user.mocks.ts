bc60e55917ba417c4133fd5a0cbea8d746c0a7d0
Gowri Shankar <shankar.chari@gmail.com>
2017-06-18 23:54:16 +0530

    Profile display

--- src/mocks/user.mocks.ts
+++ src/mocks/user.mocks.ts
@@ -6,14 +6,14 @@ const userList: User[] = [
         company: 'PWH',
         location: 'Durham, UK',
         bio: 'I make videos on my favorite technologies, Go Angular!',
-        avatar_url: 'http://i.imgur.com/java62p6.jpg',
+        avatar_url: 'https://avatars1.githubusercontent.com/u/637224?v=3&s=40',
         email: 'paul@paul.com'
     }, {
         name: 'ShankarChari',
         company: 'Ivy',
         location: 'Chennai, India',
         bio: 'I watch videos of my favorite technologies, Go Ionic',
-        avatar_url: 'http://i.imgur.com/java62p6.jpg',
+        avatar_url: 'https://avatars1.githubusercontent.com/u/637224?v=3&s=40',
         email: 'shankar.chari@gmail.com'
     }
 ]
